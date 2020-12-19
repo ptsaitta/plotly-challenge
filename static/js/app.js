19a2b8e18eb6d1c2b74e80ef08f7415920b27b28
Patrick Saitta <ptsaitta@gmail.com>
2020-12-19 14:50:37 -0500

    loaded in data

---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+//load in data to use to build charts
+
+function buildChart(sample) {
+    //load in data from json file and assign vars
+    d3.json("samples.json").then((data) => {
+        var samples = data.samples;
+        
+    }
+}
