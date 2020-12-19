f97d9c3b6c5bd8b16796f0fdaae87bd769adaa6b
Patrick Saitta <ptsaitta@gmail.com>
2020-12-19 14:57:43 -0500

    selecting data

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,7 @@ function buildChart(sample) {
     //load in data from json file and assign vars
     d3.json("samples.json").then((data) => {
         var samples = data.samples;
-        
+        var results = samples.filter(sampleEntry => sampleEntry.id == sample);
+        var result = results[0]
     }
 }
